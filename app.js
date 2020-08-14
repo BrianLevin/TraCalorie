aa20addf4007a7b6c3fe9a50fc3c6b5556bd758c
Brian Levin <bml201095@gmail.com>
2020-08-14 15:05:30 -0400

    added a datastructor to the ItemCtrl function to hold the data

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ const ItemCtrl = (function(){
     this.id = id;
     this.name = name;
     this.calories = calories;
+ }
+ // Data Structure / State
+ const data = {
+    items: [
+      {id: 0, name: 'Steak Dinner', calories: 1200},
+      {id: 1, name: 'Cookie', calories: 400},
+      {id: 2, name: 'Eggs', calories: 300}
+    ],
+    currentItem: null,
+    totalCalories: 0
+  }
 })();
 
 
