460c8a53b8a287e78657b399b9be2c2c2db2b433
Brian Levin <bml201095@gmail.com>
2020-08-15 15:39:51 -0400

    added an event listener which will disable the  enter key when submitiing

--- app.js
+++ app.js
@@ -180,12 +180,20 @@ const App = (function(ItemCtrl, UICtrl){
 
     // Add item event
     document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
+     // Disable submit on enter
+     document.addEventListener('keypress', function(e){
+        if(e.keyCode === 13 || e.which === 13){
+          e.preventDefault();
+          return false;
+        }
+    })
 
     // Edit icon click event
     document.querySelector(UISelectors.itemList).addEventListener('click', itemEditClick);
 
     // Update item event
     document.querySelector(UISelectors.updateBtn).addEventListener('click', itemUpdateSubmit);
+
   
   }
   const itemAddSubmit=function(e){
